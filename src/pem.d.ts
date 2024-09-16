e3d805cda32b632f616fdc9652fdc0851f7cd90b
handoko <srabutdotcom@gmail.com>
2024-09-16 07:09:16 +0700

    revision

--- src/pem.d.ts
+++ src/pem.d.ts
@@ -13,37 +13,35 @@ export function pem(string: string): Pem;
 export class Pem {
     static pem: typeof pem;
     /**
-     *
      * @param {string} pemString - base64 pem string
      */
     constructor(pemString: string);
     types: {
-        'RSA PRIVATE KEY': string;
+        "RSA PRIVATE KEY": string;
         CERTIFICATE: string;
-        'RSA PUBLIC KEY': string;
-        'DSA PRIVATE KEY': string;
-        'PUBLIC KEY': string;
-        'PRIVATE KEY': string;
+        "RSA PUBLIC KEY": string;
+        "DSA PRIVATE KEY": string;
+        "PUBLIC KEY": string;
+        "PRIVATE KEY": string;
         PKCS7: string;
-        'NEW CERTIFICATE REQUEST': string;
-        'CERTIFICATE REQUEST': string;
-        'X509 CRL': string;
-        'EC PRIVATE KEY': string;
-        '(RSA |EC )?PRIVATE KEY': string;
-        '(RSA )?PUBLIC KEY': string;
+        "NEW CERTIFICATE REQUEST": string;
+        "CERTIFICATE REQUEST": string;
+        "X509 CRL": string;
+        "EC PRIVATE KEY": string;
+        "(RSA |EC )?PRIVATE KEY": string;
+        "(RSA )?PUBLIC KEY": string;
     };
     /**@type {string} string - description */
     string: string;
     /**
-        * return certificate pem string or throw TypeError
-        * @returns {pemstring|TypeError} certificate pem string
-        */
+     * return certificate pem string or throw TypeError
+     * @returns {pemstring|TypeError} certificate pem string
+     */
     certificate(): pemstring | TypeError;
     /**
-        *
-        * @param {string} type - string
-        * @returns {RegExp} the regex
-        */
+     * @param {string} type - string
+     * @returns {RegExp} the regex
+     */
     regex(type: string): RegExp;
     /**
      * return pemstring based on optional pem type otherwise throw TypeError
@@ -76,4 +74,17 @@ export type pemstring = base64;
 /**
  * various type of pemstring
  */
-export type pemtype = "RSA PRIVATE KEY" | "CERTIFICATE" | "RSA PUBLIC KEY" | "DSA PRIVATE KEY" | "PUBLIC KEY" | "PRIVATE KEY" | "PKCS7" | "NEW CERTIFICATE REQUEST" | "CERTIFICATE REQUEST" | "X509 CRL" | "EC PRIVATE KEY" | "(RSA |EC )?PRIVATE KEY" | "(RSA )?PUBLIC KEY";
+export type pemtype =
+    | "RSA PRIVATE KEY"
+    | "CERTIFICATE"
+    | "RSA PUBLIC KEY"
+    | "DSA PRIVATE KEY"
+    | "PUBLIC KEY"
+    | "PRIVATE KEY"
+    | "PKCS7"
+    | "NEW CERTIFICATE REQUEST"
+    | "CERTIFICATE REQUEST"
+    | "X509 CRL"
+    | "EC PRIVATE KEY"
+    | "(RSA |EC )?PRIVATE KEY"
+    | "(RSA )?PUBLIC KEY";
