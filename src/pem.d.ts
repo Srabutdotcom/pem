99e2e77f451ca25f674787ebd165dcd58974dc1a
handoko <srabutdotcom@gmail.com>
2024-09-16 07:05:06 +0700

    version 0.1.5

--- src/pem.d.ts
+++ src/pem.d.ts
@@ -4,17 +4,67 @@
 /**
  * input pem string and return object containing: string, types, sure, certificate, privateKey, and publicKey to return the expected type of pem string otherwise throw TypeError
  * @param {string} string - pem string
- * @return {{string: pemstring, types:pemtype, regex:(type:pemtype)=>RegExp, sure:(type:pemtype)=>pemstring, publicKey:(type:("RSA"|""))=>pemstring, privateKey:(type:("RSA"|"EC"|"DSA"|""))=>pemstring, certificate:()=>pemstring}} object containing: string, types, sure, certificate, privateKey, and publicKey
+ * @return {Pem} Pem class
  */
-export function pem(string: string): {
-    string: pemstring;
-    types: pemtype;
-    regex: (type: pemtype) => RegExp;
-    sure: (type: pemtype) => pemstring;
-    publicKey: (type: ("RSA" | "")) => pemstring;
-    privateKey: (type: ("RSA" | "EC" | "DSA" | "")) => pemstring;
-    certificate: () => pemstring;
-};
+export function pem(string: string): Pem;
+/**
+ * represent Pem string object
+ */
+export class Pem {
+    static pem: typeof pem;
+    /**
+     *
+     * @param {string} pemString - base64 pem string
+     */
+    constructor(pemString: string);
+    types: {
+        'RSA PRIVATE KEY': string;
+        CERTIFICATE: string;
+        'RSA PUBLIC KEY': string;
+        'DSA PRIVATE KEY': string;
+        'PUBLIC KEY': string;
+        'PRIVATE KEY': string;
+        PKCS7: string;
+        'NEW CERTIFICATE REQUEST': string;
+        'CERTIFICATE REQUEST': string;
+        'X509 CRL': string;
+        'EC PRIVATE KEY': string;
+        '(RSA |EC )?PRIVATE KEY': string;
+        '(RSA )?PUBLIC KEY': string;
+    };
+    /**@type {string} string - description */
+    string: string;
+    /**
+        * return certificate pem string or throw TypeError
+        * @returns {pemstring|TypeError} certificate pem string
+        */
+    certificate(): pemstring | TypeError;
+    /**
+        *
+        * @param {string} type - string
+        * @returns {RegExp} the regex
+        */
+    regex(type: string): RegExp;
+    /**
+     * return pemstring based on optional pem type otherwise throw TypeError
+     * @param {pemtype} type pem type
+     * @returns {pemstring|TypeError} pemstring
+     */
+    sure(type: pemtype): pemstring | TypeError;
+    type: string;
+    /**
+     * return publicKey pem string or throw TypeError
+     * @param {"RSA"|""} type - type
+     * @returns {pemstring|TypeError} publicKey pem string
+     */
+    publicKey(type?: "RSA" | ""): pemstring | TypeError;
+    /**
+     * return privateKey pem string or throw TypeError
+     * @param {"RSA|"DSA"|"EC"|""} type
+     * @returns {pemstring|TypeError} privateKey pem string
+     */
+    privateKey(type?: string): pemstring | TypeError;
+}
 /**
  * base64 as variant of string
  */
--- src/pem.js
+++ src/pem.js
@@ -6,73 +6,85 @@
 /**
  * input pem string and return object containing: string, types, sure, certificate, privateKey, and publicKey to return the expected type of pem string otherwise throw TypeError
  * @param {string} string - pem string
- * @return {{string: pemstring, types:pemtype, regex:(type:pemtype)=>RegExp, sure:(type:pemtype)=>pemstring, publicKey:(type:("RSA"|""))=>pemstring, privateKey:(type:("RSA"|"EC"|"DSA"|""))=>pemstring, certificate:()=>pemstring}} object containing: string, types, sure, certificate, privateKey, and publicKey
+ * @return {Pem} Pem class 
  */
 export function pem(string){
-   string = String(string);
-   return {
-      string,
-      types: Object.freeze({
-         'RSA PRIVATE KEY': 'RSA PRIVATE KEY',
-         'CERTIFICATE': 'CERTIFICATE',
-         'RSA PUBLIC KEY': 'RSA PUBLIC KEY',
-         'DSA PRIVATE KEY': 'DSA PRIVATE KEY',
-         'PUBLIC KEY': 'PUBLIC KEY',
-         'PRIVATE KEY': 'PRIVATE KEY',
-         'PKCS7': 'PKCS7',
-         'NEW CERTIFICATE REQUEST': 'NEW CERTIFICATE REQUEST',
-         'CERTIFICATE REQUEST': 'CERTIFICATE REQUEST',
-         'X509 CRL': 'X509 CRL',
-         'EC PRIVATE KEY':'EC PRIVATE KEY',
-         '(RSA |EC )?PRIVATE KEY': '(RSA |EC )?PRIVATE KEY',
-         '(RSA )?PUBLIC KEY' : '(RSA )?PUBLIC KEY'
-      }),
-      /**
+   return new Pem(string)
+}
+/**
+ * represent Pem string object
+ */
+export class Pem {
+   static pem = pem
+   types= {
+      'RSA PRIVATE KEY': 'RSA PRIVATE KEY',
+      'CERTIFICATE': 'CERTIFICATE',
+      'RSA PUBLIC KEY': 'RSA PUBLIC KEY',
+      'DSA PRIVATE KEY': 'DSA PRIVATE KEY',
+      'PUBLIC KEY': 'PUBLIC KEY',
+      'PRIVATE KEY': 'PRIVATE KEY',
+      'PKCS7': 'PKCS7',
+      'NEW CERTIFICATE REQUEST': 'NEW CERTIFICATE REQUEST',
+      'CERTIFICATE REQUEST': 'CERTIFICATE REQUEST',
+      'X509 CRL': 'X509 CRL',
+      'EC PRIVATE KEY':'EC PRIVATE KEY',
+      '(RSA |EC )?PRIVATE KEY': '(RSA |EC )?PRIVATE KEY',
+      '(RSA )?PUBLIC KEY' : '(RSA )?PUBLIC KEY'
+   }
+   /**@type {string} string - description */
+   string
+   /**
+    * 
+    * @param {string} pemString - base64 pem string 
+    */
+   constructor(pemString){
+      this.string = pemString
+   }
+   /**
+       * return certificate pem string or throw TypeError
+       * @returns {pemstring|TypeError} certificate pem string
+       */
+   certificate(){
+      return this.sure(this.types.CERTIFICATE)
+   }
+   /**
        * 
        * @param {string} type - string
        * @returns {RegExp} the regex
        */
-      regex(type){
-         return new RegExp(`^(-----BEGIN ${type}-----\r?\n?(?:[A-Za-z0-9+/=]+\r?\n?)*-----END ${type}-----)\r?\n?$`)
-      },
-      /**
-       * return pemstring based on optional pem type otherwise throw TypeError
-       * @param {pemtype} type pem type
-       * @returns {pemstring|TypeError} pemstring
-       */
-      sure(type){
-         const generalType = '.*' 
-         if(!type)this.type = generalType;
-         if(!Object.prototype.hasOwnProperty.call(this.types, type))this.type = generalType;
-         this.type = type;
-         if(this.regex(this.type).test(this.string)==false)return TypeError(`Expected PEM format ${type}`);
-         return this.string;
-      },
-      /**
-       * return publicKey pem string or throw TypeError
-       * @param {"RSA"|""} type - type
-       * @returns {pemstring|TypeError} publicKey pem string
-       */
-      publicKey(type=""){
-         type = type!=="RSA"?"(RSA )?":type+" "
-         return this.sure(type+"PUBLIC KEY")
-      },
-      /**
-       * return privateKey pem string or throw TypeError
-       * @param {"RSA|"DSA"|"EC"|""} type 
-       * @returns {pemstring|TypeError} privateKey pem string
-       */
-      privateKey(type=""){
-         type = ["RSA","DSA","EC"].includes(type)==false?"(RSA |EC |DSA )?":type+" "
-         return this.sure(type+"PRIVATE KEY")
-      },
-      /**
-       * return certificate pem string or throw TypeError
-       * @returns {pemstring|TypeError} certificate pem string
-       */
-      certificate(){
-         return this.sure(this.types.CERTIFICATE)
-      }
+   regex(type){
+      return new RegExp(`^(-----BEGIN ${type}-----\r?\n?(?:[A-Za-z0-9+/=]+\r?\n?)*-----END ${type}-----)\r?\n?$`)
+   }
+   /**
+    * return pemstring based on optional pem type otherwise throw TypeError
+    * @param {pemtype} type pem type
+    * @returns {pemstring|TypeError} pemstring
+    */
+   sure(type){
+      const generalType = '.*' 
+      if(!type)this.type = generalType;
+      if(!Object.prototype.hasOwnProperty.call(this.types, type))this.type = generalType;
+      this.type = type;
+      if(this.regex(this.type).test(this.string)==false)return TypeError(`Expected PEM format ${type}`);
+      return this.string;
+   }
+   /**
+    * return publicKey pem string or throw TypeError
+    * @param {"RSA"|""} type - type
+    * @returns {pemstring|TypeError} publicKey pem string
+    */
+   publicKey(type=""){
+      type = type!=="RSA"?"(RSA )?":type+" "
+      return this.sure(type+"PUBLIC KEY")
+   }
+   /**
+    * return privateKey pem string or throw TypeError
+    * @param {"RSA|"DSA"|"EC"|""} type 
+    * @returns {pemstring|TypeError} privateKey pem string
+    */
+   privateKey(type=""){
+      type = ["RSA","DSA","EC"].includes(type)==false?"(RSA |EC |DSA )?":type+" "
+      return this.sure(type+"PRIVATE KEY")
    }
 }
 
